a559c3ae2aed91bb9fc3bdf12325f814b08bce83
Vishnu <vprabh@pdisoftware.com>
2021-11-22 20:43:57 -0600

    done

---
+++ m6_week5_d1/factory/tablet.js
@@ -0,0 +1,8 @@
+const Tablet = function ({ ram, ssd, name, network }) {
+    this.ram = ram || 0;
+    this.ssd = ssd || 0;
+    this.name = name || "";
+    this.network = network || 0;
+};
+
+module.exports = Tablet;
